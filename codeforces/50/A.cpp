a11890d0126437d8dc875f1d1d9864a0fda03661
Satyam <satyamkt512@gmail.com>
2020-10-16 09:54:00 +0530

    Add solution for problem `A - Domino piling` Link: https://codeforces.com/contest/50/problem/A Tags: greedy, math, *800 Ref: https://codeforces.com/contest/50/submission/95631581

---
+++ codeforces/50/A.cpp
@@ -0,0 +1,21 @@
+/******************************************************************************
+
+                              Online C++ Compiler.
+               Code, Compile, Run and Debug C++ program online.
+Write your code in this editor and press "Run" button to compile and execute it.
+
+*******************************************************************************/
+
+#include <iostream>
+#include <cstring>
+
+using namespace std;
+
+int main()
+{
+    int n,m;
+    cin>>n>>m;
+    cout<<(n*m)/2;
+
+    return 0;
+}
