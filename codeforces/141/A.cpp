6606f7c8e6fc2c7e61c918e5cce774dfa8eb1a98
Satyam <satyamkt512@gmail.com>
2020-10-29 19:18:00 +0530

    Add solution for problem `A - Amusing Joke` Link: https://codeforces.com/contest/141/problem/A Tags: implementation, sortings, strings, *800 Ref: https://codeforces.com/contest/141/submission/97097515

---
+++ codeforces/141/A.cpp
@@ -0,0 +1,19 @@
+#include <bits/stdc++.h>
+using namespace std;
+int main()
+{
+    string s1,s2,s3;
+    cin>>s1>>s2>>s3;
+    s1=s1+s2;
+    sort(s1.begin(),s1.end());
+    sort(s3.begin(),s3.end());
+    sort(s2.begin(),s2.end());
+    
+    if(s1==s3)
+    cout<<"YES";
+    else
+    {
+        cout<<"NO";
+    }
+    return 0;
+}
