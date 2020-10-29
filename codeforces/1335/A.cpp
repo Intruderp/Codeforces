a663d3ae8834302d6322b947f41c5b09ef94b7ee
Satyam <satyamkt512@gmail.com>
2020-10-29 19:37:00 +0530

    Add solution for problem `A - Candies and Two Sisters` Link: https://codeforces.com/contest/1335/problem/A Tags: math, *800 Ref: https://codeforces.com/contest/1335/submission/97099416

---
+++ codeforces/1335/A.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+using namespace std;
+int main()
+{
+    int t,x;
+    cin>>t;
+    for(int i=0;i<t;i++)
+    {
+        cin>>x;
+        cout<<x-x/2-1<<endl;
+    }
+    return 0;
+}
