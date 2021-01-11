1be014962c442c460e386dabd4cbc61265802b3c
Satyam <satyamkt512@gmail.com>
2021-01-11 07:53:00 +0530

    Add solution for problem `A - Frog 1` Link: https://atcoder.jp/contests/dp/tasks/dp_a Tags: AtCoder, *100 Ref: https://atcoder.jp/contests/dp/submissions/19370401

--- atcoder/dp/A.cpp
+++ atcoder/dp/A.cpp
@@ -5,26 +5,27 @@ int frog(int h[],int n,int i)
 {
     if(i==n)
     return 0;
-    if(i>n)
-    return INT_MAX;
     if(dp[i]!=-1)
     return dp[i];
-    int cost=abs(h[i]-h[i+1])+frog(h,n,i+1);
+    int cost=(abs(h[i+1]-h[i])+frog(h,n,i+1));
+    //dp[]
     if(i+2<=n)
     {
-      cost=min(cost,(abs(h[i]-h[i+2])+frog(h,n,i+2)));
+        cost=min((abs(h[i+2]-h[i])+frog(h,n,i+2)),cost);
     }
+    //else if(i+1<n)
+   
     return dp[i]=cost;
 }
 
 int main()
 {
-    int n,i=1;
+    int n,i=0;
     cin>>n;
     int h[n+1];
     memset(dp,-1,sizeof(dp));
     for(int i=1;i<=n;i++)
         cin>>h[i];
-    cout<<frog(h,n,i);
+    cout<<frog(h,n,1);
     return 0;
 }
