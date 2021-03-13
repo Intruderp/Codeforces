38aec40bda9a762dc9703e1675c7a47b4357ae98
Satyam <satyamkt512@gmail.com>
2021-03-13 15:40:00 +0530

    Add solution for problem `B - Little Elephant and Sorting` Link: https://codeforces.com/contest/205/problem/B Tags: brute force, greedy, *1400 Ref: https://codeforces.com/contest/205/submission/109864555

---
+++ codeforces/205/B.cpp
@@ -0,0 +1,33 @@
+#include<bits/stdc++.h>
+#define vi vector<int>
+#define mp map<int,int>
+#define ump unordered_map <int,int>
+#define ll long long int
+#define pb push_back
+#define mod 1000000007
+#define int long long
+#define  loop(i,x,n) for(int i=x;i<n;i++)
+#define rloop(i,n) for(int i=n-1;i>=0;i--)
+using namespace std;
+/*************************************************************************************************/
+                                                                                                   
+signed main()
+{
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
+    cout.tie(NULL);
+    int n;
+    cin>>n;
+    int a[n];
+    loop(i,0,n)
+    cin>>a[i];
+    int ans=0,mx=0,c=0;
+    loop(i,0,n)
+    {
+        mx=max(mx,a[i]);
+        ans+=mx-a[i];
+        mx=mx-(mx-a[i]);
+    }
+    cout<<ans;
+    return 0;
+}
