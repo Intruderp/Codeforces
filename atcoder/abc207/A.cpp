6da2a7185d26b118fe350ca36b9b2b75a1eb6f17
Satyam <satyamkt512@gmail.com>
2021-06-26 17:31:00 +0530

    Add solution for problem `A - Repression` Link: https://atcoder.jp/contests/abc207/tasks/abc207_a Tags: AtCoder, *100 Ref: https://atcoder.jp/contests/abc207/submissions/23756569

---
+++ atcoder/abc207/A.cpp
@@ -0,0 +1,39 @@
+#include<bits/stdc++.h>
+#pragma GCC optimize("Ofast")
+#pragma GCC optimize ("O3")
+#pragma GCC target ("avx")
+#define vi vector<int>
+#define pii pair<int,int>
+#define vpii vector<pair<int,int>>
+#define F first
+#define S second
+#define mp map<int,int>
+#define ump unordered_map <int,int>
+#define ll long long int
+#define pb push_back
+#define mod 1000000007
+#define int long long
+#define pr(x) cout<<x<<'\n'; return ;
+#define pry  cout<<"YES\n";return ;
+#define prn  cout<<"NO\n"; return ;
+#define all(v) v.begin(),v.end()
+#define  loop(i,x,n) for(int i=x;i<n;i++)
+#define rloop(i,n) for(int i=n-1;i>=0;i--)
+#define test int t; cin>>t; while(t--)
+using namespace std;
+/*************************************************************************************************/
+void solve()
+{
+    int a[3];
+    cin>>a[0]>>a[1]>>a[2];
+    sort(a,a+3);
+    cout<<a[1]+a[2];
+} 
+signed main()
+{
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
+    cout.tie(NULL);
+    solve();
+    return 0;
+}
