3ab694f29259bb28b1fb46e612d2dce776abe4a1
Satyam <satyamkt512@gmail.com>
2021-06-29 15:34:00 +0530

    Add solution for problem `C - Circle of Monsters` Link: https://codeforces.com/contest/1334/problem/C Tags: brute force, constructive algorithms, greedy, math, *1600 Ref: https://codeforces.com/contest/1334/submission/120863875

---
+++ codeforces/1334/C.cpp
@@ -0,0 +1,66 @@
+#include<bits/stdc++.h>
+#pragma GCC optimize("Ofast")
+#pragma GCC optimize ("O3")
+#pragma GCC target ("avx")
+#define vi vector<int>
+#define pii pair<int,int>
+#define vpii vector<pair<int,int>>
+#define F first
+#define S second
+#define mp map<int,int>
+#define ump unordered_map <int,int>
+#define ll long long int
+#define pb push_back
+#define mod 1000000007
+#define int long long
+#define pr(x) cout<<x<<'\n'; return ;
+#define pry  cout<<"YES\n";return ;
+#define prn  cout<<"NO\n"; return ;
+#define all(v) v.begin(),v.end()
+#define  loop(i,x,n) for(int i=x;i<n;i++)
+#define rloop(i,n) for(int i=n-1;i>=0;i--)
+#define test int t; cin>>t; while(t--)
+using namespace std;
+/*************************************************************************************************/
+void solve()
+{
+    int n,x,y,j;
+    cin>>n;
+    int a[n+1][2];
+    for(int i=1;i<=n;i++)
+    cin>>a[i][0]>>a[i][1];
+    for(int i=1;i<=n;i++)
+    {
+        j=i+1;
+        if(j==n+1)
+        j=1;
+        x=a[j][0];
+        a[i][1]=min(a[i][1],x);
+    }
+    int ans=1e18,local_ans=0;
+    x=0,y=0;
+    for(int i=1;i<=n;i++)
+    {
+        x+=a[i][0];
+        y+=a[i][1];
+    }
+    for(int i=1;i<=n;i++)
+    {
+        local_ans=x-y;
+        j=i-1;
+        if(j==0)
+        j=n;
+        local_ans+=a[j][1];
+        ans=min(ans,local_ans);
+    }
+    pr(ans)
+} 
+signed main()
+{
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
+    cout.tie(NULL);
+    test
+    solve();
+    return 0;
+}
