782e9dbef7c78ae110417c96283fefa33f94ed92
Satyam <satyamkt512@gmail.com>
2021-07-07 20:32:00 +0530

    Add solution for problem `B - Customising the Track` Link: https://codeforces.com/contest/1543/problem/B Tags: combinatorics, greedy, math, *900 Ref: https://codeforces.com/contest/1543/submission/121595425

---
+++ codeforces/1543/B.cpp
@@ -0,0 +1,52 @@
+#include<bits/stdc++.h>
+#pragma GCC optimize("Ofast")
+#pragma GCC optimize ("O3")
+#pragma GCC target ("avx")
+#define vi vector<int>
+#define pii pair<int,int>
+#define vpii vector<pair<int,int>>
+#define F first
+#define S second
+#define mp map<int,int>
+#define ump unordered_map <int,int>
+#define ll long long int
+#define pb push_back
+#define mod 1000000007
+#define int long long
+#define pr(x) cout<<x<<'\n'; return ;
+#define pry  cout<<"YES\n";return ;
+#define prn  cout<<"NO\n"; return ;
+#define all(v) v.begin(),v.end()
+#define  loop(i,x,n) for(int i=x;i<n;i++)
+#define rloop(i,n) for(int i=n-1;i>=0;i--)
+#define test int t; cin>>t; while(t--)
+using namespace std;
+/*************************************************************************************************/
+void solve()
+{
+    int n,s=0;
+    cin>>n;
+    int x;
+    loop(i,0,n)
+    {
+        cin>>x;
+        s+=x;
+    }
+    if(s%n==0)
+    cout<<"0\n";
+    else 
+    {
+        int r=s%n;
+        x=n-r;
+        cout<<x*r<<'\n';
+    }
+} 
+signed main()
+{
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
+    cout.tie(NULL);
+    test
+    solve();
+    return 0;
+}
