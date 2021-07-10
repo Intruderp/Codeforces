db907b220be2f58cd98b4b3c60bffc39282ba464
Satyam <satyamkt512@gmail.com>
2021-07-10 17:34:00 +0530

    Add solution for problem `B - Can you buy them all?` Link: https://atcoder.jp/contests/abc209/tasks/abc209_b Tags: AtCoder, *200 Ref: https://atcoder.jp/contests/abc209/submissions/24110354

---
+++ atcoder/abc209/B.cpp
@@ -0,0 +1,50 @@
+#include<bits/stdc++.h>
+#pragma GCC optimize("Ofast")
+#pragma GCC optimize ("O3")
+#pragma GCC target ("avx")
+#define vi vector<int>
+#define pii pair<int,int>
+#define vpii vector<pair<int,int>>
+#define F first
+#define S second
+#define mp map<int,int>
+#define ump unordered_map <int,int>
+#define ll long long int
+#define pb push_back
+#define mod 1000000007
+#define int long long
+#define pr(x) cout<<x<<'\n'; return ;
+#define pry  cout<<"YES\n";return ;
+#define prn  cout<<"NO\n"; return ;
+#define all(v) v.begin(),v.end()
+#define  loop(i,x,n) for(int i=x;i<n;i++)
+#define rloop(i,n) for(int i=n-1;i>=0;i--)
+#define test int t; cin>>t; while(t--)
+using namespace std;
+/*************************************************************************************************/
+void solve()
+{
+    int n,p,x=0;
+    cin>>n>>p;
+    int a[n+1];
+    loop(i,1,n+1)
+    cin>>a[i];
+    loop(i,1,n+1)
+    {
+        x+=a[i];
+        if(i%2==0)
+        x--;
+    }
+    if(x<=p)
+    cout<<"Yes";
+    else
+    cout<<"No";
+} 
+signed main()
+{
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
+    cout.tie(NULL);
+    solve();
+    return 0;
+}
