4dc42b36dd22bb1b644824ca0069068a413312e3
Satyam <satyamkt512@gmail.com>
2021-07-12 17:47:00 +0530

    Add solution for problem `D - Collision` Link: https://atcoder.jp/contests/abc209/tasks/abc209_d Tags: AtCoder, *400 Ref: https://atcoder.jp/contests/abc209/submissions/24184570

---
+++ atcoder/abc209/D.cpp
@@ -0,0 +1,66 @@
+#include<bits/stdc++.h>
+#pragma GCC optimize("Ofast")
+#pragma GCC optimize ("O3")
+#pragma GCC target ("avx")
+#define vi vector<int>
+#define pii pair<int,int>
+#define vpii vector<pair<int,int>>
+#define F first
+#define S second
+#define mp map<int,int>
+#define ump unordered_map <int,int>
+#define ll long long int
+#define pb push_back
+#define mod 1000000007
+#define int long long
+#define pr(x) cout<<x<<'\n'; return ;
+#define pry  cout<<"YES\n";return ;
+#define prn  cout<<"NO\n"; return ;
+#define all(v) v.begin(),v.end()
+#define  loop(i,x,n) for(int i=x;i<n;i++)
+#define rloop(i,n) for(int i=n-1;i>=0;i--)
+#define test int t; cin>>t; while(t--)
+using namespace std;
+/*************************************************************************************************/
+int n,q;
+vector<int> v[100000+1];
+int vis[100000+1];
+int col[100000+1];
+void dfs(int node,int c)
+{
+    vis[node]=1;
+    col[node]=c;
+    for(int child:v[node])
+    {
+        if(vis[child]==0)
+        dfs(child,c^1);
+    }
+}
+void solve()
+{
+    cin>>n>>q;
+    int a,b;
+    loop(i,0,n-1)
+    {
+        cin>>a>>b;
+        v[a].pb(b);
+        v[b].pb(a);
+    }
+    dfs(1,0);
+    while(q--)
+    {
+        cin>>a>>b;
+        if(col[a]==col[b])
+        cout<<"Town\n";
+        else 
+        cout<<"Road\n";
+    }
+} 
+signed main()
+{
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
+    cout.tie(NULL);
+    solve();
+    return 0;
+}
