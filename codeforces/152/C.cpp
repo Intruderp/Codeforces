75104ceae9f0500f267098814d98ac3d76750feb
Satyam <satyamkt512@gmail.com>
2021-07-17 11:17:00 +0530

    Add solution for problem `C - Pocket Book` Link: https://codeforces.com/contest/152/problem/C Tags: combinatorics, *1400 Ref: https://codeforces.com/contest/152/submission/122736408

---
+++ codeforces/152/C.cpp
@@ -0,0 +1,56 @@
+#include<bits/stdc++.h>
+#pragma GCC optimize("Ofast")
+#pragma GCC optimize ("O3")
+#pragma GCC target ("avx")
+#define vi vector<int>
+#define pii pair<int,int>
+#define vpii vector<pair<int,int>>
+#define F first
+#define S second
+#define mp map<int,int>
+#define ump unordered_map <int,int>
+#define ll long long int
+#define pb push_back
+#define mod 1000000007
+#define int long long
+#define pr(x) cout<<x<<'\n'; return ;
+#define pry  cout<<"YES\n";return ;
+#define prn  cout<<"NO\n"; return ;
+#define all(v) v.begin(),v.end()
+#define  loop(i,x,n) for(int i=x;i<n;i++)
+#define rloop(i,n) for(int i=n-1;i>=0;i--)
+#define test int t; cin>>t; while(t--)
+using namespace std;
+/*************************************************************************************************/
+void solve()
+{
+    int n,m,ans=1;
+    cin>>n>>m;
+    vector<string> v(n);
+    loop(i,0,n)
+    cin>>v[i];
+    loop(i,0,m)
+    {
+        vi freq(26,0);
+        loop(j,0,n)
+        freq[v[j][i]-'A']++;
+        int f=0;
+        for(int &i:freq)
+        {
+            if(i)
+            f++;
+        }
+        ans=(ans*f)%mod;
+    }
+    pr(ans)
+
+} 
+signed main()
+{
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
+    cout.tie(NULL);
+    //test
+    solve();
+    return 0;
+}
