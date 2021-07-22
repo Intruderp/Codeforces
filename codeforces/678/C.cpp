804f594196bf7cd3fd58fa235e4a9c6d4bb91189
Satyam <satyamkt512@gmail.com>
2021-07-22 15:54:00 +0530

    Add solution for problem `C - Joty and Chocolate` Link: https://codeforces.com/contest/678/problem/C Tags: implementation, math, number theory, *1600 Ref: https://codeforces.com/contest/678/submission/123264910

---
+++ codeforces/678/C.cpp
@@ -0,0 +1,48 @@
+#include<bits/stdc++.h>
+#pragma GCC optimize("Ofast")
+#pragma GCC optimize ("O3")
+#pragma GCC target ("avx")
+#define vi vector<int>
+#define pii pair<int,int>
+#define vpii vector<pair<int,int>>
+#define F first
+#define S second
+#define mp map<int,int>
+#define ump unordered_map <int,int>
+#define ll long long int
+#define pb push_back
+#define mod 1000000007
+#define int long long
+#define pr(x) cout<<x<<'\n'; return ;
+#define pry  cout<<"YES\n";return ;
+#define prn  cout<<"NO\n"; return ;
+#define all(v) v.begin(),v.end()
+#define  loop(i,x,n) for(int i=x;i<n;i++)
+#define rloop(i,n) for(int i=n-1;i>=0;i--)
+#define test int t; cin>>t; while(t--)
+using namespace std;
+/*************************************************************************************************/
+int lcm(int a,int b)
+{
+    return a*b/(__gcd(a,b));
+}
+void solve()
+{
+    int n,a,b,p,q,z;
+    cin>>n>>a>>b>>p>>q;
+    int x1=n/a,x2=n/b;
+    z=lcm(a,b);
+    if(p>q)
+    x2-=n/z;
+    else
+    x1-=n/z;
+    pr(x1*p+x2*q)
+} 
+signed main()
+{
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
+    cout.tie(NULL);
+    solve();
+    return 0;
+}
