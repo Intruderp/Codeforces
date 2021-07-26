7a1735c456593ec6521d7d1cc0749d4cec962200
Satyam <satyamkt512@gmail.com>
2021-07-26 20:59:00 +0530

    Add solution for problem `F - Consecutive Subsequence` Link: https://codeforces.com/contest/977/problem/F Tags: dp, *1700 Ref: https://codeforces.com/contest/977/submission/123853787

---
+++ codeforces/977/F.cpp
@@ -0,0 +1,60 @@
+#include<bits/stdc++.h>
+#pragma GCC optimize("Ofast")
+#pragma GCC optimize ("O3")
+#pragma GCC target ("avx")
+#define vi vector<int>
+#define pii pair<int,int>
+#define vpii vector<pair<int,int>>
+#define F first
+#define S second
+#define mp map<int,int>
+#define ump unordered_map <int,int>
+#define ll long long int
+#define pb push_back
+#define mod 1000000007
+#define int long long
+#define pr(x) cout<<x<<'\n'; return ;
+#define pry  cout<<"YES\n";return ;
+#define prn  cout<<"NO\n"; return ;
+#define all(v) v.begin(),v.end()
+#define  loop(i,x,n) for(int i=x;i<n;i++)
+#define rloop(i,n) for(int i=n-1;i>=0;i--)
+#define test int t; cin>>t; while(t--)
+using namespace std;
+/*************************************************************************************************/
+void solve()
+{
+    int n,len=0,start=0;
+    map<int,int> dp;
+    cin>>n;
+    int a[n];
+    loop(i,0,n)
+    cin>>a[i];
+    loop(i,0,n)
+    {
+        int x=a[i];
+        dp[x]=max(dp[x],dp[x-1]+1);
+        if(dp[x]>len)
+        {
+            len=dp[x];
+            start=x-len+1;
+        }
+    }
+    cout<<len<<'\n';
+    loop(i,0,n)
+    {
+        if(a[i]==start)
+        {
+            cout<<i+1<<' ';
+            start++;
+        }
+    }
+} 
+signed main()
+{
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
+    cout.tie(NULL);
+    solve();
+    return 0;
+}
