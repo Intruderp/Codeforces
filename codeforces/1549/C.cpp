060241a5f434316ce31772bf53a432177bd18d5b
Satyam <satyamkt512@gmail.com>
2021-08-02 08:58:00 +0530

    Add solution for problem `C - Web of Lies` Link: https://codeforces.com/contest/1549/problem/C Tags: graphs, greedy, *1400 Ref: https://codeforces.com/contest/1549/submission/124637226

---
+++ codeforces/1549/C.cpp
@@ -0,0 +1,81 @@
+#include <bits/stdc++.h>
+#pragma GCC optimize("Ofast")
+#pragma GCC optimize("O3")
+#pragma GCC target("avx")
+#define vi vector<int>
+#define pii pair<int, int>
+#define vpii vector<pair<int, int>>
+#define F first
+#define S second
+#define mp map<int, int>
+#define ump unordered_map<int, int>
+#define ll long long int
+#define pb push_back
+#define mod 1000000007
+#define int long long
+#define pr(x)          \
+    cout << x << '\n'; \
+    return;
+#define pry          \
+    cout << "YES\n"; \
+    return;
+#define prn         \
+    cout << "NO\n"; \
+    return;
+#define all(v) v.begin(), v.end()
+#define loop(i, x, n) for (int i = x; i < n; i++)
+#define rloop(i, n) for (int i = n - 1; i >= 0; i--)
+#define test  \
+    int t;    \
+    cin >> t; \
+    while (t--)
+using namespace std;
+/*************************************************************************************************/
+void solve()
+{
+    int n, x, a, b, q, t, ans = 0;
+    ump m;
+    cin >> n >> x;
+    loop(i, 0, x)
+    {
+        cin >> a >> b;
+        if (a > b)
+            m[b]++,ans+=m[b]==1;
+        else
+            m[a]++,ans+=m[a]==1;
+    }
+    cin >> q;
+    while (q--)
+    {
+        cin >> t;
+        if (t == 3)
+        {
+            cout << n - ans << '\n';
+        }
+        else if (t == 1)
+        {
+            cin >> a >> b;
+            if (a > b)
+                m[b]++, ans += m[b] == 1;
+            else
+                m[a]++, ans += m[a] == 1;
+        }
+        else
+        {
+            cin >> a >> b;
+            if (a > b)
+                m[b]--, ans -= m[b] == 0;
+            else
+                m[a]--, ans -= m[a] == 0;
+        }
+    }
+}
+signed main()
+{
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
+    cout.tie(NULL);
+    //test
+    solve();
+    return 0;
+}
